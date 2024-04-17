ab2c6e28991536f2bca704a236f9d25b71a9783a
Anssi Mäkinen <amakinen@student.hive.fi>
2024-04-17 14:18:42 +0300

    Add missing prototypes for ft_calloc and ft_strdup

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 17:18:27 by amakinen          #+#    #+#             */
-/*   Updated: 2024/04/17 11:59:20 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/04/17 14:16:14 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 #endif
