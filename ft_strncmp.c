3ce01dd9fe314ea0672fcfc218f2b4c9fac63c69
Anssi Mäkinen <amakinen@student.hive.fi>
2024-04-22 16:03:00 +0300

    ft_strncmp: Fix incorrect loop condition

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 11:18:44 by amakinen          #+#    #+#             */
-/*   Updated: 2024/04/17 11:32:05 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/04/22 15:50:32 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	cmp;
 
 	cmp = 0;
-	while (*s1 && cmp && n--)
+	while (*s1 && cmp == 0 && n--)
 		cmp = (unsigned char)*s1++ - (unsigned char)*s2++;
 	return (cmp);
 }
