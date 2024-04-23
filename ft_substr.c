2136c75585e615124af358f504b7267b25cd8b62
Anssi Mäkinen <amakinen@student.hive.fi>
2024-04-23 16:15:29 +0300

    ft_substr: Do nothing with start past string end

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:27:42 by amakinen          #+#    #+#             */
-/*   Updated: 2024/04/18 13:23:23 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/04/23 16:15:01 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	s_len;
 	char	*sub;
 
-	s += start;
 	s_len = ft_strlen(s);
+	if (start > s_len)
+		s_len = 0;
+	else
+	{
+		s += start;
+		s_len -= start;
+	}
 	if (s_len < len)
 		len = s_len;
 	sub = malloc(len + 1);
