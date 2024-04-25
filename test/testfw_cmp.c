e78a3741f564b5b8ab88f2983203a953f970041b
Anssi Mäkinen <amakinen@student.hive.fi>
2024-04-25 15:32:37 +0300

    Convert macros to functions in memcmp and strncmp tests

--- test/testfw_cmp.c
+++ test/testfw_cmp.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 15:09:47 by amakinen          #+#    #+#             */
-/*   Updated: 2024/04/24 16:59:09 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/04/25 16:03:56 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,58 @@
 #include <string.h>
 #include <stdio.h>
 
-#define MATCHCMP(fn, s1, s2, n) \
-	ft_r = ft_##fn(s1, s2, n); \
-	std_r = fn(s1, s2, n); \
-	if (ft_r != std_r) \
-	{ \
-		TEST_FAIL("ft_" #fn "(\"%s\", \"%s\", %d) failed: " \
-			"ft_ returned %d, std returned %d\n", \
-			s1, s2, n, ft_r, std_r); \
-	}
+static void	match_memcmp(char *s1, char *s2, size_t n)
+{
+	int	ft_r;
+	int	std_r;
 
-#define TESTCMP(n) \
-REGISTER_TEST(ft_##n);\
-\
-static void	test_ft_##n(void)\
-{\
-	int ft_r;\
-	int std_r;\
-\
-	MATCHCMP(n, "abc", "abc", 4)\
-	MATCHCMP(n, "abc", "abd", 4)\
-	MATCHCMP(n, "abc", "abd", 2)\
-	MATCHCMP(n, "\0abc", "\0abd", 3)\
-	MATCHCMP(n, "\0abc", "\0abd", 4)\
-	MATCHCMP(n, "a\0bc", "a\0bd", 4)\
-	MATCHCMP(n, "a\0bc", "b\0bc", 4)\
+	ft_r = ft_memcmp(s1, s2, n);
+	std_r = memcmp(s1, s2, n);
+	if (ft_r != std_r)
+	{
+		TEST_FAIL("ft_memcmp(\"%s\", \"%s\", %zu) failed: "
+			"ft_ returned %d, std returned %d\n",
+			s1, s2, n, ft_r, std_r);
+	}
 }
 
-TESTCMP(memcmp);
-TESTCMP(strncmp);
+REGISTER_TEST(ft_memcmp);
+
+static void	test_ft_memcmp(void)
+{
+	match_memcmp("abc", "abc", 4);
+	match_memcmp("abc", "abd", 4);
+	match_memcmp("abc", "abd", 2);
+	match_memcmp("\0abc", "\0abd", 3);
+	match_memcmp("\0abc", "\0abd", 4);
+	match_memcmp("a\0bc", "a\0bd", 4);
+	match_memcmp("a\0bc", "b\0bc", 4);
+}
+
+static void	match_strncmp(char *s1, char *s2, size_t n)
+{
+	int	ft_r;
+	int	std_r;
+
+	ft_r = ft_strncmp(s1, s2, n);
+	std_r = strncmp(s1, s2, n);
+	if (ft_r != std_r)
+	{
+		TEST_FAIL("ft_strncmp(\"%s\", \"%s\", %zu) failed: "
+			"ft_ returned %d, std returned %d\n",
+			s1, s2, n, ft_r, std_r);
+	}
+}
+
+REGISTER_TEST(ft_strncmp);
+
+static void	test_ft_strncmp(void)
+{
+	match_strncmp("abc", "abc", 4);
+	match_strncmp("abc", "abd", 4);
+	match_strncmp("abc", "abd", 2);
+	match_strncmp("\0abc", "\0abd", 3);
+	match_strncmp("\0abc", "\0abd", 4);
+	match_strncmp("a\0bc", "a\0bd", 4);
+	match_strncmp("a\0bc", "b\0bc", 4);
+}
