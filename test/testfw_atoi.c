f357baa3c11740a3e176148c172ab0d4d519659b
Anssi Mäkinen <amakinen@student.hive.fi>
2024-04-25 15:32:18 +0300

    Convert macro to function in atoi test

--- test/testfw_atoi.c
+++ test/testfw_atoi.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 17:39:33 by amakinen          #+#    #+#             */
-/*   Updated: 2024/04/24 16:59:02 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/04/25 11:34:31 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,40 +16,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define MATCHATOI(s) \
-	ft_r = ft_atoi(s); \
-	std_r = atoi(s); \
-	if (ft_r != std_r) \
-	{ \
-		TEST_FAIL("ft_atoi(\"%s\") failed: " \
-			"ft_ returned %d, std returned %d\n", \
-			s, ft_r, std_r); \
+static void	match_atoi(const char *s)
+{
+	int	ft_r;
+	int	std_r;
+
+	ft_r = ft_atoi(s);
+	std_r = atoi(s);
+	if (ft_r != std_r)
+	{
+		TEST_FAIL("ft_atoi(\"%s\") failed: "
+			"ft_ returned %d, std returned %d\n",
+			s, ft_r, std_r);
 	}
+}
 
 REGISTER_TEST(ft_atoi);
 
 static void	test_ft_atoi(void)
 {
-	int	ft_r;
-	int	std_r;
-
-	MATCHATOI("abc");
-	MATCHATOI("abc99");
-	MATCHATOI("99abc");
-	MATCHATOI("9abc9");
-	MATCHATOI("a99bc");
-	MATCHATOI("0");
-	MATCHATOI("1");
-	MATCHATOI("-1");
-	MATCHATOI("+1");
-	MATCHATOI("--1");
-	MATCHATOI("-+1");
-	MATCHATOI("+-1");
-	MATCHATOI("++1");
-	MATCHATOI("999");
-	MATCHATOI("-999");
-	MATCHATOI("2147483647");
-	MATCHATOI("2147483646");
-	MATCHATOI("-2147483647");
-	MATCHATOI("-2147483648");
+	match_atoi("abc");
+	match_atoi("abc99");
+	match_atoi("99abc");
+	match_atoi("9abc9");
+	match_atoi("a99bc");
+	match_atoi("0");
+	match_atoi("1");
+	match_atoi("-1");
+	match_atoi("+1");
+	match_atoi("--1");
+	match_atoi("-+1");
+	match_atoi("+-1");
+	match_atoi("++1");
+	match_atoi("999");
+	match_atoi("-999");
+	match_atoi("2147483647");
+	match_atoi("2147483646");
+	match_atoi("-2147483647");
+	match_atoi("-2147483648");
 }
