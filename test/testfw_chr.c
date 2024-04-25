142f573e17f90254fbbd923df0f858431d210340
Anssi Mäkinen <amakinen@student.hive.fi>
2024-04-25 16:37:28 +0300

    Split testfw_search and replace macros with functions

---
+++ test/testfw_chr.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testfw_chr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 16:05:44 by amakinen          #+#    #+#             */
+/*   Updated: 2024/04/25 16:31:09 by amakinen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "testfw.h"
+#include "libft.h"
+#include <string.h>
+
+static void	match_strchr(int reverse, const char *s, int c)
+{
+	char	*ft_r;
+	char	*std_r;
+	char	*fn;
+
+	if (reverse)
+	{
+		ft_r = ft_strrchr(s, c);
+		std_r = strrchr(s, c);
+		fn = "strrchr";
+	}
+	else
+	{
+		ft_r = ft_strchr(s, c);
+		std_r = strchr(s, c);
+		fn = "strchr";
+	}
+	if (ft_r != std_r)
+		TEST_FAIL("ft_%s(\"%s\", '%c'<%d>) failed: "
+			"ft_ returned \"%s\" (%p), std returned \"%s\" (%p)\n",
+			fn, s, c, c, ft_r, ft_r, std_r, std_r);
+}
+
+REGISTER_TEST(strchr);
+
+static void	test_strchr(void)
+{
+	match_strchr(0, "abc", 'a');
+	match_strchr(0, "abc", 'b');
+	match_strchr(0, "abc", 'c');
+	match_strchr(0, "abc", '\0');
+	match_strchr(0, "abc", 'd');
+	match_strchr(0, "abc", 'A');
+}
+
+REGISTER_TEST(strrchr);
+
+static void	test_strrchr(void)
+{
+	match_strchr(1, "abc", 'a');
+	match_strchr(1, "abc", 'b');
+	match_strchr(1, "abc", 'c');
+	match_strchr(1, "abc", '\0');
+	match_strchr(1, "abc", 'd');
+	match_strchr(1, "abc", 'A');
+}
+
+static void	match_memchr(const char *s, int c, size_t n)
+{
+	char	*ft_r;
+	char	*std_r;
+
+	ft_r = ft_memchr(s, c, n);
+	std_r = memchr(s, c, n);
+	if (ft_r != std_r)
+		TEST_FAIL("ft_memchr(\"%s\", '%c'<%d>, %zu) failed: "
+			"ft_ returned \"%s\" (%p), std returned \"%s\" (%p)\n",
+			s, c, c, n, ft_r, ft_r, std_r, std_r);
+}
+
+REGISTER_TEST(ft_memchr);
+
+static void	test_ft_memchr(void)
+{
+	match_memchr("abc", 'a', 4);
+	match_memchr("abc", 'b', 4);
+	match_memchr("abc", 'c', 4);
+	match_memchr("abc", '\0', 4);
+	match_memchr("abc", '\0', 3);
+	match_memchr("abc", 'd', 4);
+	match_memchr("abc", 'A', 4);
+}
---
+++ test/testfw_strnstr.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testfw_strnstr.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 15:03:35 by amakinen          #+#    #+#             */
+/*   Updated: 2024/04/25 15:14:20 by amakinen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "testfw.h"
+#include "libft.h"
+#include <string.h>
+
+static void	match_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	char	*ft_r;
+	char	*std_r;
+
+	std_r = strnstr(haystack, needle, len);
+	ft_r = ft_strnstr(haystack, needle, len);
+	if (ft_r != std_r)
+		TEST_FAIL("ft_strnstr(\"%s\", \"%s\", %zu) failed: "
+			"ft_ returned \"%s\" (%p), std returned \"%s\" (%p)\n",
+			haystack, needle, len, ft_r, ft_r, std_r, std_r);
+}
+
+REGISTER_TEST(ft_strnstr);
+
+static void	test_ft_strnstr(void)
+{
+	match_strnstr("abcd", "abcd", 4);
+	match_strnstr("abcd", "abcd", 3);
+	match_strnstr("abcd", "bc", 4);
+	match_strnstr("abcd", "bc", 3);
+	match_strnstr("abcd", "bc", 2);
+	match_strnstr("abcd", "cd", 4);
+	match_strnstr("abcd", "cd", 3);
+	match_strnstr("abcd", "a", 5);
+	match_strnstr("abcd", "a", 1);
+	match_strnstr("abcd", "a", 0);
+	match_strnstr("\0abcd", "a", 5);
+	match_strnstr("abcd", "", 5);
+	match_strnstr("abcd", "", 1);
+	match_strnstr("abcd", "", 0);
+	match_strnstr("abcd", "\0a", 4);
+	match_strnstr("abcd", "\0a", 1);
+	match_strnstr("abcd", "\0a", 0);
+	match_strnstr("abcd", "abcde", 5);
+	match_strnstr("abcd", "abcde", 4);
+	match_strnstr("abcd", "abce", 4);
+	match_strnstr("abcd", "abce", 3);
+	match_strnstr("abcd", "cb", 4);
+	match_strnstr(0, "", 1);
+	match_strnstr(0, "", 0);
+}
