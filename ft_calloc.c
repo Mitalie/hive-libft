457622512cab350e1cba5785891da470dfc30c9d
Anssi Mäkinen <amakinen@student.hive.fi>
2024-04-30 13:21:28 +0300

    ft_atoi, ft_calloc, ft_memcpy, ft_memmove: Better match libc behaviour
    Some of these requirements are questionable, but strict interpretation
    of libft assignment requires exactly matching the libc functions'
    behaviour. There is allowance for undefined behaviour for crashes, but
    it's not clear whether this only applies when libc function would also
    crash, and whether this applies even if system manpages don't explicitly
    state the behaviour is undefined.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:23:22 by amakinen          #+#    #+#             */
-/*   Updated: 2024/04/17 12:43:43 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/04/30 13:11:02 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,22 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+/*
+	C allows implementations to decide whether to return a NULL pointer or a
+	unique freeable pointer for zero size allocations. The libft assignment
+	requires matching the libc function behaviour, so we avoid filtering zero
+	sizes with an early return and pass them on to libc malloc.
+
+	Some libft testers explicitly require non-NULL pointers, which this
+	implementation would fail if malloc returns NULL, but if that happens it's
+	likely libc calloc would also return NULL and therefore the test is wrong.
+*/
+
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	if (!count || !size || count > SIZE_MAX / size)
+	if (size && count > SIZE_MAX / size)
 		return (0);
 	ptr = malloc(count * size);
 	if (ptr)
