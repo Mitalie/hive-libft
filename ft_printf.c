aa4e4d5f007a4c64928f36ae0303611f6c3b9a38
Anssi Mäkinen <amakinen@student.hive.fi>
2024-05-06 16:19:47 +0300

    Implement plaintext printing and format specifier detection

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,57 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 11:28:27 by amakinen          #+#    #+#             */
-/*   Updated: 2024/05/06 13:00:05 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/05/06 16:13:37 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <limits.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+static bool	handle_specifier(const char **fmt, int *written, va_list *args)
+{
+	(void)written;
+	(void)args;
+	(*fmt)++;
+	return (false);
+}
+
+static bool	print_until_specifier(const char **fmt, int *written)
+{
+	size_t	len;
+
+	len = 0;
+	while ((*fmt)[len] && (*fmt)[len] != '%')
+		len++;
+	if (len > (unsigned)INT_MAX - *written)
+		return (false);
+	if (write(STDOUT_FILENO, *fmt, len) < 0)
+		return (false);
+	*fmt += len;
+	*written += len;
+	return (true);
+}
 
 int	ft_printf(const char *fmt, ...)
 {
-	(void)fmt;
-	return (-1);
+	va_list	args;
+	bool	success;
+	int		written;
+
+	va_start(args, fmt);
+	written = 0;
+	while (*fmt)
+	{
+		if (*fmt == '%')
+			success = handle_specifier(&fmt, &written, &args);
+		else
+			success = print_until_specifier(&fmt, &written);
+		if (!success)
+			return (-1);
+	}
+	va_end(args);
+	return (written);
 }
