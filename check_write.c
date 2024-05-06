bb80f19449f35fc7d88cb3199208ac4000f50402
Anssi Mäkinen <amakinen@student.hive.fi>
2024-05-06 18:26:20 +0300

    Handle EINTR and short writes

--- check_write.c
+++ check_write.c
@@ -6,18 +6,36 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 18:03:41 by amakinen          #+#    #+#             */
-/*   Updated: 2024/05/06 18:06:55 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/05/06 18:26:02 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf_internal.h"
+#include <errno.h>
 #include <limits.h>
 #include <unistd.h>
+
+static bool	write_retry(int fd, const void *buf, size_t len)
+{
+	ssize_t	ret;
+
+	while (len)
+	{
+		ret = write(fd, buf, len);
+		if (ret < 0 && errno == EINTR)
+			continue ;
+		else if (ret < 0)
+			return (false);
+		buf = (char *)buf + ret;
+		len -= ret;
+	}
+	return (true);
+}
 
 bool	check_write(int *written, const void *data, size_t len)
 {
 	if (len > (unsigned)INT_MAX - *written)
 		return (false);
 	*written += len;
-	return (write(STDOUT_FILENO, data, len) >= 0);
+	return (write_retry(STDOUT_FILENO, data, len));
 }
