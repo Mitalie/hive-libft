5c3f9d99c4bdb99b18c3002d3ade465a00ba25b4
Anssi Mäkinen <amakinen@student.hive.fi>
2024-05-16 15:53:27 +0300

    Read from file for test, for easier debugging

--- test/test_main.c
+++ test/test_main.c
@@ -6,10 +6,11 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 15:50:41 by amakinen          #+#    #+#             */
-/*   Updated: 2024/05/13 18:51:11 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/05/16 15:49:37 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 #include "get_next_line.h"
@@ -18,14 +19,16 @@ int	main(void)
 {
 	size_t	linenbr;
 	char	*line;
+	int		fd;
 
 	linenbr = 0;
-	printf("Reading stdin line-by-line...\n");
-	line = get_next_line(STDIN_FILENO);
+	fd = open("test.txt", O_RDONLY);
+	printf("Reading test.txt line-by-line...\n");
+	line = get_next_line(fd);
 	while (line)
 	{
 		printf("%5zu: %s", ++linenbr, line);
-		line = get_next_line(STDIN_FILENO);
+		line = get_next_line(fd);
 	}
 	printf("\nGot %zu lines\n", linenbr);
 }
